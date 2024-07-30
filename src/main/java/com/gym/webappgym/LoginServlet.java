d28e36be8b692cda03946d515c4206fa26a227ed
rajeshrajan <rajeshawsconnect@gmail.com>
2024-07-30 13:09:13 +0530

    login changes

--- src/main/java/com/gym/webappgym/LoginServlet.java
+++ src/main/java/com/gym/webappgym/LoginServlet.java
@@ -1,5 +1,9 @@
 package com.gym.webappgym;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import org.json.JSONObject;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,6 +21,30 @@ public class LoginServlet extends HttpServlet {
 
         // Get the RequestDispatcher for the target resource
         RequestDispatcher dispatcher = request.getRequestDispatcher("/home.jsp");
+
+        try{
+
+            HttpResponse<String> loginResponse = Unirest.post("http://localhost:8763/login")
+                    .header("Content-Type", "application/json")
+                    .body("{\"username\":\"test\",\"password\":\"test\"}")
+                    .asString();
+
+            JSONObject respJson = new JSONObject(loginResponse.getBody());
+            if(respJson.getBoolean("status")){
+
+                JSONObject gymOwnerJson = respJson.getJSONObject("gymowner");
+                System.out.println("gymOwnerJson"+gymOwnerJson);
+                request.setAttribute("name", gymOwnerJson.getString("name"));
+
+            }else {
+
+                dispatcher = request.getRequestDispatcher("/index.jsp");
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
 
         // Include the output of the includedPage.jsp in the response
         dispatcher.include(request, response);
